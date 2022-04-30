ea3c010d2834dd0718d08c449e66bf91c8cbc510
Anup <haldaranup746@gmail.com>
2022-04-30 17:28:30 +0530

    add sidebar hamburger menu

--- src/components/menu/Menu.jsx
+++ src/components/menu/Menu.jsx
@@ -5,16 +5,16 @@ export default function Menu({ menuOpen, setMenuOpen }) {
     <div className={"menu "+(menuOpen && "active")}>
       <ul>
         <li onClick={()=>setMenuOpen(false)}>
-          <a href="#intro">Home</a>
+          <a href="#intro">About</a>
         </li>
         <li onClick={()=>setMenuOpen(false)}>
-          <a href="#portfolio">Portfolio</a>
+          <a href="#skills">Skills</a>
         </li>
         <li onClick={()=>setMenuOpen(false)}>
           <a href="#works">Works</a>
         </li>
         <li onClick={()=>setMenuOpen(false)}>
-          <a href="#testimonials">Testimonials</a>
+          <a href="#resume">Resume</a>
         </li>
         <li onClick={()=>setMenuOpen(false)}>
           <a href="#contact">Contact</a>
